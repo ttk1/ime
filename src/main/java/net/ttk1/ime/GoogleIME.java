25b686bff7d2b68d327fa3a96648194918c80cc5
tama <tama@ttk1.net>
2020-09-20 03:05:38 +0900

    一応動くようになった。

--- src/main/java/net/ttk1/ime/GoogleIME.java
+++ src/main/java/net/ttk1/ime/GoogleIME.java
@@ -1,25 +1,34 @@
 package net.ttk1.ime;
 
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+
 import javax.net.ssl.HttpsURLConnection;
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GoogleIME {
+    private static final Gson gson = new Gson();
+
     public static String transliterate(String text) throws IOException {
         URL url = new URL(String.format("https://www.google.com/transliterate?langpair=ja-Hira|ja&text=%s", URLEncoder.encode(text, "UTF-8")));
         HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
         conn.connect();
         if (conn.getResponseCode() == HttpsURLConnection.HTTP_OK) {
-            int BUFF_SIZE = 100;
-            byte[] buf = new byte[BUFF_SIZE];
-            InputStream is = conn.getInputStream();
             StringBuilder sb = new StringBuilder();
-            int len;
-            while ((len = is.read(buf, 0, BUFF_SIZE)) >= 0) {
-                sb.append(new String(buf, 0, len));
+            JsonReader jr = new JsonReader(new InputStreamReader(conn.getInputStream()));
+            // [[word1,[candidate1,candidate2,...]],[word2,...]]
+            // ↑ みたいな感じで入ってくるので、`List<List<List<String>>>` は本当はよくない
+            List<List<List<String>>> words = new ArrayList<>();
+            words = gson.fromJson(jr, words.getClass());
+            for (List<List<String>> word : words) {
+                List<String> candidates = word.get(1);
+                sb.append(candidates.get(0));
             }
             return sb.toString();
         } else {
